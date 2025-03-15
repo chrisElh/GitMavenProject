d11dbc076dfc5d13a1480f57b946b1a2a96df96c
chrisElh <christian-eh@hotmail.com>
2025-03-15 19:37:43 +0100

    changes in the main file to try a recall

--- GitMavenProjectJ/src/main/java/org/example/Main.java
+++ GitMavenProjectJ/src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello, this is the featureBr branch!");
+        for (int i = 0; i <= 10; i++){
+            System.out.printf("%d ", i);
+        }
 
     }
 }
